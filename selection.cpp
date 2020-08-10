12ac44bed670ab901a77528a0c3adace9aa149e5
riyazjahangir <j.mohammedriyaz181@google.com>
2020-08-10 05:01:23 +0000

    first commit

---
+++ selection.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void sort(int* x[],int n){
+}
+
+int main(){
+	int a[5];
+	cout<<"\nEnter 5 numbers : ";
+	for(int i = 0;i<5;i++){
+		cin>>a[i];
+	}
+	int n = sizeof(a)/sizeof(int);
+	return 0;
+}
